bfe133cf7c7b846c5ad76851133aab7911347dd2
herbert henrique <bert@pop-os.localdomain>
2021-10-22 12:24:40 -0300

    front-end: adicionei estados aos inputs do form, irei trabalhar com controlled components

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 
+import { useState } from 'react';
 import FormGroup from '../FormGroup';
 
 import { Form, ButtonContainer } from './styles';
@@ -9,30 +10,56 @@ import Select from '../select';
 import Button from '../button';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({
+      name, email, phone, category,
+    });
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
 
       <FormGroup>
-        <Input placeholder="Nome" />
+        <Input
+          placeholder="Nome"
+          onChange={(event) => setName(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup error="Este e-mail não é válido">
-        <Input placeholder="E-mail" error />
+        <Input
+          placeholder="E-mail"
+          error
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Telefone"
+          onChange={(event) => setPhone(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select placeholder="Categoria">
-          <option value="123" selected>Categoria</option>
+        <Select
+          placeholder="Categoria"
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="123">Categoria</option>
           <option value="321">Instagram</option>
         </Select>
       </FormGroup>
 
       <ButtonContainer>
-        <Button>
+        <Button type="submit">
           {buttonLabel}
         </Button>
       </ButtonContainer>
