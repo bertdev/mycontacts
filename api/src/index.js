4a3f2760c0bbf292667fa18720506793dff67fa3
herbert henrique <herbertmatos10@hotmail.com>
2021-10-27 21:01:24 -0300

    api: adicionando middleware de cors

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('./app/middlewares/cors');
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(routes);
 app.use((error, request, response, next) => {
